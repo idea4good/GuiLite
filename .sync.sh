2f0efa113122b8544c3ce42e2d96eedc6844cb82
idea4good <idea4good@outlook.com>
2018-10-26 17:42:52 +0800

    update sync

--- .sync.sh
+++ .sync.sh
@@ -1,9 +1,9 @@
-url="https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/f56a3240-7d0c-45aa-a694-65ebf6a04cc2/rows?key=RHtUJLk6Vi7QIlOBAPENJuCHOT6WbVdBi%2Fa%2BhwoDFpaOFhTsPH8zXWE%2F%2FFSjPdxF7Lmkf%2Fl1q2nLgBYk7HYqUQ%3D%3D"
-build_time=`date +%Y-%m-%dT%H:%M:%S`
+url="https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/2ff1e8a8-2f6f-4d73-a75d-86829e3f4574/rows?key=8f5xLp1gP8%2FzSee4vCUBcyjR65I9zZ6nb%2B%2F7bbzex%2FSctLX3ntIlAR0sxWpDdguuYyDtLdHK%2Fxbxj%2FrSBkX7eQ%3D%3D"
+build_time=`date +%Y-%m-%dT%H:%M:%S.000+0800`
 device_info=`uname -s -n -m`
 curl --include --request POST --header "Content-Type: application/json" --data-binary "[{
-\"Build time\" :\"$build_time\",
-\"Device info\" :\"$device_info\",
-\"Project info\" :\"$1\",
-\"Weight\" :1
+\"device_info\" :\"$device_info\",
+\"project_info\" :\"$1\",
+\"time\" :\"$build_time\",
+\"weight\" :1
 }]" $url
--- sync.bat
+++ sync.bat
@@ -1,15 +1,15 @@
-::echo off
-set url="https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/f56a3240-7d0c-45aa-a694-65ebf6a04cc2/rows?key=RHtUJLk6Vi7QIlOBAPENJuCHOT6WbVdBi%%2Fa%%2BhwoDFpaOFhTsPH8zXWE%%2F%%2FFSjPdxF7Lmkf%%2Fl1q2nLgBYk7HYqUQ%%3D%%3D"
+echo off
+set url="https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/2ff1e8a8-2f6f-4d73-a75d-86829e3f4574/rows?key=8f5xLp1gP8%%2FzSee4vCUBcyjR65I9zZ6nb%%2B%%2F7bbzex%%2FSctLX3ntIlAR0sxWpDdguuYyDtLdHK%%2Fxbxj%%2FrSBkX7eQ%%3D%%3D"
 set date=
 for /f "tokens=2-4 delims=/ "  %%a in ("%date%") do (set MM=%%a& set DD=%%b& set YYYY=%%c)
-set datetime=%YYYY%-%MM%-%DD%T%time%
-set devie_info=Windows-%USERNAME%
+set datetime=%YYYY%-%MM%-%DD%T%time: =0%0+0800
+set devie_info=Win-%USERNAME%
 
 set raw_data=[{^
-\"Build time\" :\"%datetime%\",^
-\"Device info\" :\"%devie_info%\",^
-\"Project info\" :\"%1\",^
-\"Weight\" : 1^
+\"device_info\" :\"%devie_info%\",^
+\"project_info\" :\"%1\",^
+\"time\" :\"%datetime%\",^
+\"weight\" : 1^
 }]
 
 curl.exe --include --request POST --header "Content-Type: application/json" --data-binary^
