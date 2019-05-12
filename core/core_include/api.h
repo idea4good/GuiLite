c46a5d309b21de02a8af8ea68d4dc026e74d7da9
idea4good <idea4good@outlook.com>
2019-05-13 02:23:32 +0800

    fix bitmap issue on 32 bits linux

--- core/core_include/api.h
+++ core/core_include/api.h
@@ -23,7 +23,7 @@ void log_out(const char* log);
 #define GL_RGB_G(rgb) ((((unsigned int)(rgb)) >> 8) & 0xFF)
 #define GL_RGB_B(rgb) (((unsigned int)(rgb)) & 0xFF)
 #define GL_RGB_32_to_16(rgb) (((((unsigned int)(rgb)) & 0xFF) >> 3) | ((((unsigned int)(rgb)) & 0xFC00) >> 5) | ((((unsigned int)(rgb)) & 0xF80000) >> 8))
-#define GL_RGB_16_to_32(rgb) (((((unsigned int)(rgb)) & 0x1F) << 3) | ((((unsigned int)(rgb)) & 0x7E0) << 5) | ((((unsigned int)(rgb)) & 0xF800) << 8))
+#define GL_RGB_16_to_32(rgb) ((0xFF << 24) | ((((unsigned int)(rgb)) & 0x1F) << 3) | ((((unsigned int)(rgb)) & 0x7E0) << 5) | ((((unsigned int)(rgb)) & 0xF800) << 8))
 
 typedef struct _T_TIME
 {
